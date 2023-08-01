8e6daf5a10bc4a4b6072893637ab49869e9ccefe
Matheus Calixto Muniz <caalixto@live.com>
2023-08-01 14:57:09 -0300

    #03 - Laços Condicionais - Exercício 02 - Condicional IF

---
+++ CondicionalIf02.java
@@ -0,0 +1,28 @@
+package JavaTech;
+
+//2) - Escreva um algoritmo em Java, que leia um número inteiro via teclado e mostre na tela uma mensagem indicando se este número é par ou ímpar e se o número é positivo ou negativo. 
+import java.util.Scanner;
+
+public class CondicionalIf02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner read = new Scanner(System.in);
+		int numero; 
+		System.out.print("Digite um número: ");
+		numero = read.nextInt();
+		
+		if ((numero % 2) == 0){
+			System.out.printf("O número %d é par ", numero);
+		} else {
+			System.out.printf("O número %d é ímpar ", numero);
+		}
+		if (numero >= 0) {
+			System.out.printf("e positivo!");
+		} else {
+			System.out.printf("e negativo!");
+		}
+		
+	}
+
+}
